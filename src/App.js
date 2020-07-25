f8bc97aa441f79a56bf4b88b43ab048c945070b8
imran9945 <62025696+imran9945@users.noreply.github.com>
2020-07-25 23:21:56 +0500

    UPDATED REACT STATE

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
-
-function App() {
-  let [count , setCount] = useState(0)
-
+import {text} from './text.js';
+export default function App() {
+  let [count , setCount] = useState(1)
+  let [lightoff , setlightoff] = useState(false)
   return (
-    <div>
-      <h3>VALUE OF COUNT IS ::  {count}</h3>
+    <div className={`box ${lightoff ? 'nightlight': ''}`}>
+      <h2>NIGHT LIGHT IS {lightoff ? 'ON' : 'OFF'}</h2>
+    <text>counter={count}</text>
+    <br></br>
+    <button onClick={()=> setCount(++count)}>UPDATE COUNTER</button>
+    <button onClick={()=>setlightoff(!lightoff)}>TURN NIGHT LIGHT ON/OFF</button>
     </div>
   );
 }
 
-export default App;
---
+++ src/text.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export function text (props){
+return(
+    <h3>VALUE OF COUNT IS ::  {props.counter}</h3>
+)
+}
